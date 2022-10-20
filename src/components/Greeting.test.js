dd9a96fb93b179afef11667e508890b6f59bd702
Harry Dhillon <hdhillon478@gmail.com>
2022-10-20 20:05:58 +0100

    Move Greeting test under test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("Render Hello World as text", () => {
-  // Arrange - Import the components beings tested
-  render(<Greeting />);
+// Grouping related component tests in  one suite
+describe("Greeting Component", () => {
+  test("Render Hello World as text", () => {
+    // Arrange - Import the components beings tested
+    render(<Greeting />);
 
-  // Act - Execute functions to run tests
-  // ...nothing
+    // Act - Execute functions to run tests
+    // ...nothing
 
-  // Assert - What is the expected result ? What do you expect to see ?
-  const element = screen.getByText(/hello world/i);
-  expect(element).toBeInTheDocument();
+    // Assert - What is the expected result ? What do you expect to see ?
+    const element = screen.getByText(/hello world/i);
+    expect(element).toBeInTheDocument();
+  });
 });
