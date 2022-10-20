6ffd337a07240e71fac88a9ebb32ab2a7d7ad51a
Harry Dhillon <hdhillon478@gmail.com>
2022-10-20 20:44:00 +0100

    Add tests with user events on button click

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 // Grouping Tests together with a Test Suite
 describe("Greeting Component", () => {
-  test("Render Hello World as text", () => {
+  test("renders 'Hello World' as text", () => {
     // Arrange - Import the components beings tested
     render(<Greeting />);
 
@@ -15,11 +16,36 @@ describe("Greeting Component", () => {
     expect(element).toBeInTheDocument();
   });
 
-  test("Render text prior to button click", () => {
+  test("renders 'It's good to see you' text prior to button click", () => {
     render(<Greeting />);
 
     // Test to see if text can be found withing the component
     const screenText = screen.getByText(/It's good to see you/i);
     expect(screenText).toBeInTheDocument();
+  });
+
+  test("renders 'Text has been changed! WOW!!!!' after the button has been clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const screenText = screen.getByText(/Text has been changed! WOW!!!!/i);
+    expect(screenText).toBeInTheDocument();
+  });
+
+  test("does not render 'It's good to see you' after the button has been clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // queryByText will return NULL if the text is NOT found
+    const screenText = screen.queryByText(/It's good to see you/i);
+    // we expect the result to be null because the text should not be on the screen
+    expect(screenText).toBeNull();
   });
 });
