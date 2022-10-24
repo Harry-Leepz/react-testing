b4ca659d60c35695f45ff79573468aa603f891b8
Harry Dhillon <hdhillon478@gmail.com>
2022-10-24 11:54:33 +0100

    Add test suit for Async component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Greeting from "./components/Greeting";
+import Async from "./components/Async";
+
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
       <Greeting />
+      <hr />
+      <Async />
     </div>
   );
 }
